88d20218f550edd0e42744c71f4792688ee299e2
Giougt <alexandre18golf@gmail.com>
2024-11-19 11:22:47 +0100

    add file save and method

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,3 +1,16 @@
-import {export_time} from "./app/run.py";
-const test = export_time();
-console.log(test);  
+/*import {export_time} from "./app/run.py";
+//const test = export_time();
+console.log(test);  */
+const fs = require ("fs"); 
+const saveAs = require('file-saver');
+
+const filepath = "./app/backend/save_comments.txt"; 
+fs.writeFile(filepath,"hello");
+
+
+/*
+const textToWrite = 'Hello, World!';
+const blob = new Blob([textToWrite], { type: 'text/plain' });
+saveAs(blob, 'example.txt');
+*/
+
