2732cfc5f7deb2136604ce6dccac3994d592d281
Giougt <alexandre18golf@gmail.com>
2024-11-20 18:58:09 +0100

    first sucess only script not save

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,14 +1,26 @@
-import write_file from "../module/write";
 
 const entry = document.createElement("input");
 document.body.appendChild(entry);
+
+const add= document.createElement("button");
+add.innerText= "valide";
+add.id="validate"
+document.body.appendChild(add);
 
 Object.assign(entry.style, {
     backgroundColor: "black",
     color: "white"
 });
-//// part script 
-// possible erreur ==> module ! 
+Object.assign(add.style,{
+    backgroundColor: "red",
+    color: "white"
+});
+
+document.getElementById("validate").addEventListener("click", function() {
+    let b= document.createElement("p");
+    b.append(entry.value);
+    document.body.appendChild(b);
+})
 
 console.log("hello");
 
