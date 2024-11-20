62d949a360afef88be8fa3ed7b3fa4c6aa3ecfe2
Giougt <alexandre18golf@gmail.com>
2024-11-20 23:30:24 +0100

    add color input and try to catch error

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,11 +1,14 @@
-
+//input text
 const entry = document.createElement("input");
-document.body.appendChild(entry);
 
+//button send texte and color 
 const add= document.createElement("button");
 add.innerText= "valide";
 add.id="validate"
-document.body.appendChild(add);
+
+//settup color 
+const changeColor = document.createElement("input");
+changeColor.type = "color";
 
 Object.assign(entry.style, {
     backgroundColor: "black",
@@ -16,13 +19,30 @@ Object.assign(add.style,{
     color: "white"
 });
 
+//append in document part 
+document.body.appendChild(entry);
+document.body.appendChild(changeColor);
+document.body.appendChild(add);
+
+// get , add , check value  
 document.getElementById("validate").addEventListener("click", function() {
+    try{
+        if (/^[a-zA-Z\s]+$/.test(entry.value) === false){
+            throw new Error ("not only letter"); 
+        }
+    }catch (error){
+        console.error("error is detect", error.message); 
+    }
+    if (entry.value !== ""){
     let b= document.createElement("p");
     b.append(entry.value);
     document.body.appendChild(b);
+    entry.value = "";
+    }
+    else{
+        console.error("field is empty")
+    }
 })
-
-console.log("hello");
 
 
 
