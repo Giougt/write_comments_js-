352f569fc9c30d2ceec2b4ab1fb9ae147128d8c1
Giougt <alexandre18golf@gmail.com>
2024-11-26 22:47:38 +0100

    add keyPress event on input

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,5 +1,6 @@
 //input text
 const entry = document.createElement("input");
+entry.id="input_Field";
 
 //button send texte and color 
 const add= document.createElement("button");
@@ -24,31 +25,17 @@ document.body.appendChild(entry);
 document.body.appendChild(changeColor);
 document.body.appendChild(add);
 
-// get , add , check value  
-document.getElementById("validate").addEventListener("click", function() {
-    try {
-        if (entry.value === "") {
-            throw new Error("field is empty");
-        }
-        if (/^[a-zA-Z\s]+$/.test(entry.value) === false) {
-            entry.value = "";
-            throw new Error("not only letters");
-        }
-        let fieldText = document.createElement("p");
-        let fieldTime = document.createElement("p");
-        // append text in page
-        fieldText.append(entry.value);
-        document.body.appendChild(fieldText);
-        //reset 
-        entry.value = "";
-        //append time in page
-        fieldTime.append(Timet_today());
-        document.body.appendChild(fieldTime);
-
-    } catch (error) {
-        console.error("Error detected:", error.message);
+// call function
+document.getElementById("validate").addEventListener("click", add_comment);
+//press enter
+document.getElementById("input_Field").addEventListener("keypress",function(event){
+    if (event.key === "Enter") {
+        //
+        event.preventDefault();
+        //call function
+        add_comment();
     }
-})
+});
 
 function Timet_today() {
     let today = new Date();
@@ -64,5 +51,33 @@ function Timet_today() {
     return tab2
 };
 
+function add_comment() {
+    try {
+        if (entry.value === "") {
+            throw new Error("field is empty");
+        }
+        if (/^[a-zA-Z\s]+$/.test(entry.value) === false) {
+            entry.value = "";
+            throw new Error("not only letters");
+        }
+        let fieldText = document.createElement("div");
+        let fieldTime = document.createElement("div");
+        let fieldAll = document.createElement("div");
+        // append text in page
+        fieldText.append(entry.value);
+        fieldAll.appendChild(fieldText);
+        //reset 
+        entry.value = "";
+        //append time in page
+        fieldTime.append(Timet_today());
+        fieldAll.appendChild(fieldTime);
+        //style 
+        fieldAll.style.display = 'flex';
+        fieldAll.style.gap = '10px';
+        document.body.appendChild(fieldAll);
+    } catch (error) {
+        console.error("Error detected:", error.message);
+    }
+};
 
 
